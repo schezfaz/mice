cdabbfe2972db67801b2a60ee529419a1d076418
Schezeen Fazulbhoy <schezeenf10@gmail.com>
2020-04-26 01:00:48 +0530

    implemented keyboard interrupt to exit script using threading

--- mouse.py
+++ mouse.py
@@ -1,16 +1,26 @@
 from random import randint
 import time
+import keyboard
+import threading as th
 from datetime import datetime, timedelta
 import pyautogui as pg
 
 pg.FAILSAFE = True #drag mouse to lhs top corner to exit script
 
+run_script = True
+def exit_code():
+    global run_script
+    key_pressed = keyboard.read_key()
+    if key_pressed == "esc":
+        print("Exiting code")
+        run_script = False
+
 def idle_mouse(time_period, time_interval):
+    th.Thread(target=exit_code, args=(), name='exit_code', daemon=True).start()
     width, height = pg.size()  #get screen resolution
     print(width,height)
-
     end_time = datetime.now() + timedelta(seconds=time_period)
-    while datetime.now() < end_time:
+    while run_script and datetime.now() < end_time:
         current_position = pg.position()  # get current location of mouse
         print(current_position)
         x, y = randint(0, width - 1), randint(0, height - 1)
